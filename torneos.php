9ad9134a6b96f75eb30539b020dd71a43b6820f4
Martin Mauro Zucchiatti <tanomartin05@gmail.com>
2016-02-24 19:42:37 -0300

    subida de control de select en el index

--- torneos.php
+++ torneos.php
@@ -1,7 +1,18 @@
 <?php 
 include_once "include/templateEngine.inc.php";
+include_once 'model/torneos.php';
+include_once "model/torneos.categorias.php";
+
+$idTorneo = $_GET['idTorneo'];
+$idCatTor = $_GET['idTorneoCat'];
+
+$oTorneo = new Torneos();
+$torneo = $oTorneo->get($idTorneo);
+$oTorneoCat = new TorneoCat();
+$aTorneoCat = $oTorneoCat->get($idTorneo);
+
 
 // Cargo la plantilla
-$twig->display('torneos.html');
+$twig->display('torneos.html', array('torneo'=>$torneo[0], 'categorias' => $aTorneoCat,));
 
 ?>
