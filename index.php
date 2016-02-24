bf4b7231a33c29fc7b30e75969b20672bb973b01
tanomartin <tanomartin05@gmail.com>
2016-02-24 14:55:27 -0300

    redireccion con datos al entrar.

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 include_once 'include/config.inc.php';
 include_once 'include/templateEngine.inc.php';
 include_once 'model/torneos.php';
+include_once 'model/sedes.php';
 $oTorneos = new Torneos();
 $torneos = $oTorneos->get();
 
+$oSedes = new Sedes();
+$sedes = $oSedes->get();
+
 // Cargo la plantilla
-$twig->display('index.html', array("torneos"=> $torneos) );
+$twig->display('index.html', array("torneos"=> $torneos,"sedes"=> $sedes));
 
 ?>
