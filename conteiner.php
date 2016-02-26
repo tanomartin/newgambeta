660a885af7f0951309c9bebe3023c42b0b590a05
tanomartin <tanomartin05@gmail.com>
2016-02-26 15:42:55 -0300

    hacemos la consulta en el torneos.php para que caiga en las pantalla con todo cargado de la base

--- conteiner.php
+++ conteiner.php
@@ -3,37 +3,12 @@ include_once "include/templateEngine.inc.php";
 include_once 'model/torneos.php';
 include_once "model/torneos.categorias.php";
 
-$idTorneo = $_POST ['idTorneo'];
-$idTorneoCat = $_POST ['idTorneoCat'];
-
-$oTorneo = new Torneos ();
-$atorneo = $oTorneo->get ( $idTorneo );
-$oTorneoCat = new TorneoCat ();
-$aTorneoCat = $oTorneoCat->getByTorneoSub ( $idTorneo );
-
-$index = 0;
-foreach ( $aTorneoCat as $categoria ) {
-	$nombreCompleto = $oTorneoCat->getCategoriasCompletas ( $categoria ['id'] );
-	$nombreCompleto = $nombreCompleto [0];
-	if ($nombreCompleto ['nombreCatPagina'] == NULL) {
-		$nombreCategoria = $nombreCompleto ['nombrePagina'];
-	} else {
-		$nombreCategoria = $nombreCompleto ['nombreCatPagina'] . " - " . $nombreCompleto ['nombrePagina'];
-	}
-	$aTorneoCat [$index] ['nombre'] = $nombreCategoria;
-	if ($categoria ['id'] == $idTorneoCat) {
-		$nombreCategoriaSelect = $nombreCategoria;
-	}
-	
-	$index ++;
-}
+$torneo = unserialize($_POST['torneo']);
+$categorias = unserialize($_POST['categorias']);
 
 // Cargo la plantilla
 $twig->display ( 'conteiner.html', array (
-		'torneo' => $atorneo [0],
-		'categorias' => $aTorneoCat,
-		'nombreCategoria' => $nombreCategoriaSelect,
-		'idTorneoCat' => $idTorneoCat,
-		'torneoseria' => serialize ( $atorneo [0] ) 
+		'torneo' => $torneo,
+		'categorias' => $categorias
 ));
 ?>
--- torneos.php
+++ torneos.php
@@ -1,10 +1,40 @@
 <?php 
 include_once "include/templateEngine.inc.php";
+include_once 'model/torneos.php';
+include_once "model/torneos.categorias.php";
 
-$idTorneo = $_POST['idTorneo'];
-$idTorneoCat = $_POST['idTorneoCat'];
+$idTorneo = $_POST ['idTorneo'];
+$idTorneoCat = $_POST ['idTorneoCat'];
+
+$oTorneo = new Torneos ();
+$atorneo = $oTorneo->get ( $idTorneo );
+$oTorneoCat = new TorneoCat ();
+$aTorneoCat = $oTorneoCat->getByTorneoSub ( $idTorneo );
+
+$index = 0;
+foreach ( $aTorneoCat as $categoria ) {
+	$nombreCompleto = $oTorneoCat->getCategoriasCompletas ( $categoria ['id'] );
+	$nombreCompleto = $nombreCompleto [0];
+	if ($nombreCompleto ['nombreCatPagina'] == NULL) {
+		$nombreCategoria = $nombreCompleto ['nombrePagina'];
+	} else {
+		$nombreCategoria = $nombreCompleto ['nombreCatPagina'] . " - " . $nombreCompleto ['nombrePagina'];
+	}
+	$aTorneoCat [$index] ['nombre'] = $nombreCategoria;
+	if ($categoria ['id'] == $idTorneoCat) {
+		$nombreCategoriaSelect = $nombreCategoria;
+	}
+	$index ++;
+}
+
 
 // Cargo la plantilla
-$twig->display('torneos.html', array('idTorneo'=>$idTorneo, 'idTorneoCat' => $idTorneoCat));
+$twig->display ( 'torneos.html', array (
+		'idTorneo' => $idTorneo,
+		'idTorneoCat' => $idTorneoCat,
+		'torneo' => serialize ( $atorneo [0] ),
+		'categorias' => serialize ( $aTorneoCat ),
+		'nombreCategoria' => $nombreCategoriaSelect
+) );
 
 ?>
