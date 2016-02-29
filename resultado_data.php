611b4444c0eff888e8fd691c2ffda1318e199d56
tanomartin <tanomartin05@gmail.com>
2016-02-29 11:29:49 -0300

    agregado de acciones en los botones faltantes, agregado de - cuando el partido no tiene resultado

--- resultado_data.php
+++ resultado_data.php
@@ -8,10 +8,20 @@ if (isset ($_POST ['idEquipoTorneo']) &&  $_POST ['idEquipoTorneo'] != 0) {
 	if ($partidos != NULL) {
 		$resultado = "<tbody>";
 		foreach ( $partidos as $partido ) {
+			if ($partido['golesEquipo1'] == -1) {
+				$goles1 = "-";
+			} else {
+				$goles1 = $partido['golesEquipo1'];
+			}
+			if ($partido['golesEquipo2'] == -1) {
+				$goles2 = "-";
+			} else {
+				$goles2 = $partido['golesEquipo2'];
+			}
 			$resultado.= "<tr>
 							  <td width='40%'>".$partido['equipo1']."</td>
-							  <td width='10%'>".$partido['golesEquipo1']."</td>
-							  <td width='10%'>".$partido['golesEquipo2']."</td>
+							  <td width='10%'>".$goles1."</td>
+							  <td width='10%'>".$goles2."</td>
 							  <td width='40%'>".$partido['equipo2']."</td>
 						  </tr>";
 		}
