85cd45aa20e7e15e319cf9e46ae415bf9da628db
tanomartin <tanomartin05@gmail.com>
2016-03-10 12:08:57 -0300

    sacamos columnas en mobile en fixture

--- fecha_data.php
+++ fecha_data.php
@@ -7,18 +7,29 @@ if (isset ($_POST ['idFecha']) &&  $_POST ['idFecha'] != 0) {
 	$oFixture = new Fixture ();
 	$partidos = $oFixture->getByFecha ( $_POST ['idFecha']);
 	if ($partidos != NULL) {
-		$resultado = "<thead>
-						<tr class='principal-table' style='background-color: ".$_POST ['color']."' >
-							<th><span class='ion-pound'></span></th>
-							<th><span class='ion-ios-location'></span></th>
-							<th><span class='ion-calendar'></span></th>
-							<th><span class='ion-android-time'></span></th>
-							<th><span class='ion-ios-people'></span></th>
-							<th colspan='2'><span class='ion-ios-football'></th>
-							<th><span class='ion-ios-people'></span></th>
-							<th></th>
-						</tr>
-					 </thead>";
+		if (isset($_POST['screen'])) {
+			$resultado = "<thead>
+							<tr class='principal-table' style='background-color: ".$_POST ['color']."' >
+								<th><span class='ion-ios-location'></span></th>
+								<th><span class='ion-calendar'></span></th>
+								<th><span class='ion-ios-people'></span></th>
+								<th><span class='ion-ios-people'></span></th>
+							</tr>
+						 </thead>";
+		} else {
+			$resultado = "<thead>
+							<tr class='principal-table' style='background-color: ".$_POST ['color']."' >
+								<th><span class='ion-pound'></span></th>
+								<th><span class='ion-ios-location'></span></th>
+								<th><span class='ion-calendar'></span></th>
+								<th><span class='ion-android-time'></span></th>
+								<th><span class='ion-ios-people'></span></th>
+								<th colspan='2'><span class='ion-ios-football'></th>
+								<th><span class='ion-ios-people'></span></th>
+								<th></th>
+							</tr>
+						 </thead>";
+		}
 		$resultado .= "<tbody>";
 		$n = 1;
 		foreach ( $partidos as $partido ) {
@@ -32,7 +43,15 @@ if (isset ($_POST ['idFecha']) &&  $_POST ['idFecha'] != 0) {
 			} else {
 				$goles2 = $partido['golesEquipo2'];
 			}
-			$resultado.= "<tr style='font-size: 16px'>
+			if (isset($_POST['screen'])) {
+				$resultado.= "<tr style='font-size: 16px'>
+							  <td>".$partido['sede']."</td>
+							  <td>".substr(cambiaf_a_normal($partido['fechaPartido']),0,2)."/".substr(cambiaf_a_normal($partido['fechaPartido']),3,2)." - ".$partido['horaPartido']."</td>
+							  <td>".$partido['equipo1']."</td>
+							  <td>".$partido['equipo2']."</td>
+							  </tr>";
+			} else {
+				$resultado.= "<tr style='font-size: 16px'>
 							  <td>".$n."</td>
 							  <td>".$partido['sede']."</td>
 							  <td>".cambiaf_a_normal($partido['fechaPartido'])."</td>
@@ -40,13 +59,10 @@ if (isset ($_POST ['idFecha']) &&  $_POST ['idFecha'] != 0) {
 							  <td>".$partido['equipo1']."</td>
 							  <td>".$goles1."</td>
 							  <td>".$goles2."</td>
-							  <td>".$partido['equipo2']."</td>";
-			if (isset($_POST['screen'])) {
-			    $resultado.= "<td><button onclick='detallepartidoMobile(".$partido['id'].")' style='background-color: ".$_POST ['color']."'><span title='Ver Detalle' class='ion-document-text'></span></button>";
-			} else {
-				$resultado.= "<td><button onclick='detallepartido(".$partido['id'].")' style='background-color: ".$_POST ['color']."'><span title='Ver Detalle' class='ion-document-text'></span></button>";
+							  <td>".$partido['equipo2']."</td>
+							  <td><button onclick='detallepartido(".$partido['id'].")' style='background-color: ".$_POST ['color']."'><span title='Ver Detalle' class='ion-document-text'></span></button>
+							  </tr>";
 			}
-			$resultado.= "</tr>";
 			$n++;
 		}
 		$resultado .= "</tbody>";
