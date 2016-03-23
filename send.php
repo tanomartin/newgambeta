d0640b824e967d866415399f74e540dee24b659d
tanomartin <tanomartin05@gmail.com>
2016-03-23 10:47:27 -0300

    envio de consultas desde el index y desde contacto

--- send.php
+++ send.php
@@ -1,23 +1,22 @@
 <?php
-error_reporting(0);
 
-//$casilla_destino = "info@gambetafemenina.com.ar";
-$casilla_destino = "mzuccotti93@gmail.com";
+$casilla_destino = "info@gambetafemenina.com.ar";
 $nombre = (isset($_POST["nombre"]))? $_POST["nombre"] : "";
 $email = (isset($_POST["email"]))? $_POST["email"] : "";
 $telefono = (isset($_POST["telefono"]))? $_POST["telefono"] : "";
 $mensaje = (isset($_POST["mensaje"]))? $_POST["mensaje"] : "";
-	
-	
+			
 $asunto = "Consulta - Gambeta Femenina";
-$cuerpo_mail .= "Nombre y Apellido: ".$nombre."\r\n";
+$cuerpo_mail = "Nombre y Apellido: ".$nombre."\r\n";
 $cuerpo_mail .= "Email: ".$email."\r\n";
 $cuerpo_mail .= "Telefono: ".$telefono."\r\n";
 $cuerpo_mail .= "Mensaje: ".$mensaje."\r\n";
-	
-	
-@mail($casilla_destino, $asunto, $cuerpo_mail);
+$cabecera = 'From: '.$email."\r\n".'Reply-To:'.$email."\r\n".'X-Mailer: PHP/'.phpversion();
 
-echo 'mensaje enviado correctamente';
+if (mail($casilla_destino, $asunto, utf8_decode($cuerpo_mail), $cabecera)) {
+	echo '1';
+} else {
+	echo '0';
+}
 
 ?>
